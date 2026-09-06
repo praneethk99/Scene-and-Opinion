5cedfc0a61c5bcb54e28799b07e75bd85409946f
praneethk99 <praneethkoushik111213@gmail.com>
2026-09-06 22:12:11 +0530

    Initial commit

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Scene and Opinion';
+export const SITE_DESCRIPTION = 'Fresh reviews before the popcorn cools!';
